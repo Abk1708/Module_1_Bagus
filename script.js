ade6f8353f33219fb3ef9c26a12b87b75e714ae9
Abk1708 <136345015+Abk1708@users.noreply.github.com>
2023-10-27 19:54:05 +0700

    Delete script.js
    Following script usability has been deprecated.

--- script.js
+++
@@ -1,3 +0,0 @@
-const submitButton = () => {
-    alert("Thank you for providing your important information. Although it quite bewilder me the fact that you couldn't see that this is not a legit website. Anyhow, Your prize will be delivered to you in 31th November. :)");
-}
